eabd90b8af657740858cee9ea7fa2bb8dbb50b0b
Achref <medachrefg@gmail.com>
2022-05-15 00:40:25 +0100

    understanding portals

--- src/shared/components/navigation/NavLinks.js
+++ src/shared/components/navigation/NavLinks.js
@@ -6,9 +6,7 @@ const NavLinks = () => {
 	return (
 		<ul className='nav-links'>
 			<li>
-				<NavLink to='/' exact>
-					All users
-				</NavLink>
+				<NavLink to='/'>All users</NavLink>
 			</li>
 			<li>
 				<NavLink to='/u1/places'>My places</NavLink>
