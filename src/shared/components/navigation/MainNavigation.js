fa33da43e50fc3bad7b3a19269dc0b5c3c3d8723
Achref <medachrefg@gmail.com>
2022-05-14 23:45:42 +0100

    implementing basic destop&mob navigation

--- src/shared/components/navigation/MainNavigation.js
+++ src/shared/components/navigation/MainNavigation.js
@@ -2,20 +2,31 @@ import React from 'react';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+
 import './MainNavigation.css';
 const MainNavigation = () => {
 	return (
-		<MainHeader>
-			<button className='main-navigation__menu-btn'>
-				<span />
-				<span />
-				<span />
-			</button>
-			<h1 className='main-navigation__title'>
-				<Link to='/'>yourPlaces</Link>
-			</h1>
-			<NavLinks />
-		</MainHeader>
+		<>
+			<SideDrawer>
+				<nav className='main-navigation__drawer-nav'>
+					<NavLinks />
+				</nav>
+			</SideDrawer>
+			<MainHeader>
+				<button className='main-navigation__menu-btn'>
+					<span />
+					<span />
+					<span />
+				</button>
+				<h1 className='main-navigation__title'>
+					<Link to='/'>your Places</Link>
+				</h1>
+				<nav className='main-navigation__header-nav'>
+					<NavLinks />
+				</nav>
+			</MainHeader>
+		</>
 	);
 };
 
