9197e6888e93ba98204ee632ead12d3f41af3dcf
Achref <medachrefg@gmail.com>
2022-05-15 01:27:30 +0100

    Animation the sidedrawers

--- src/shared/components/navigation/MainNavigation.js
+++ src/shared/components/navigation/MainNavigation.js
@@ -9,27 +9,28 @@ import Backdrop from '../UIElement/BackDrop';
 const MainNavigation = () => {
 	const [drawerIsopen, setDrawerIsOpen] = useState();
 
-	const openDrawer = () => {
+	const openDrawerHandler = () => {
 		setDrawerIsOpen(true);
 	};
 
-	const closeDrawer = () => {
+	const closeDrawerHandler = () => {
 		setDrawerIsOpen(false);
 	};
 
 	return (
 		<>
-			{drawerIsopen && <Backdrop onClick={closeDrawer} />}
-			{drawerIsopen && (
-				<SideDrawer>
-					<nav className='main-navigation__drawer-nav'>
-						<NavLinks />
-					</nav>
-				</SideDrawer>
-			)}
+			{drawerIsopen && <Backdrop onClick={closeDrawerHandler} />}
+
+			<SideDrawer show={drawerIsopen} onClick={closeDrawerHandler}>
+				<nav className='main-navigation__drawer-nav'>
+					<NavLinks />
+				</nav>
+			</SideDrawer>
 
 			<MainHeader>
-				<button className='main-navigation__menu-btn' onClick={openDrawer}>
+				<button
+					className='main-navigation__menu-btn'
+					onClick={openDrawerHandler}>
 					<span />
 					<span />
 					<span />
